b0cba47d5dfdbb7648c3613438a3b431edbf1931
Yair Zafrany <karai.wot@gmail.com>
2022-10-17 23:31:12 +0300

    Add project files.

---
+++ Models/Message.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace Yad2.Models
+{
+    public class Message
+    {
+        [Required]
+        public int MessageId { get; set; }
+
+        [Required]
+        public string AssetType { get; set; }
+
+        [Required]
+        public string AssetCondition { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Street { get; set; }
+
+        [Required]
+        public int Floor { get; set; }
+
+        [Required]
+        public int OverallNumberOfFloors { get; set; }
+
+        //public Boolean OnPillars { get; set; }
+
+        [Required]
+        public int NumberOfRooms { get; set; }
+
+        [Required]
+        public int ParkingSpots { get; set; }
+
+        [Required]
+        public int NumberOfPorches { get; set; }
+
+        public List<AssetProperties> AssetProperties { get; set; }
+
+        public int BuiltSquareMeters { get; set; }
+
+        [Required]
+        public int TotalBuildSquareMeters { get; set; }
+
+        public int Price { get; set; }
+
+
+    }
+}
